ed9168c144d2bbb115a8ae2bf63d9ebf9e6209e1
seonpilKim <kimsunpil123@naver.com>
2021-07-30 06:05:15 +0900

    에라토스테네스의 체 ps

---
+++ Sieve of Eratosthenes/boj/1929/src.cpp
@@ -0,0 +1,64 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <math.h>
+#include <bitset>
+#include <stack>
+#include <queue>
+#include <math.h>
+#include <deque>
+#include <map>
+#include <set>
+#include <unordered_set>
+#include <unordered_map>
+#include <limits.h>
+#include <numeric>
+#include <functional>
+#include <sstream>
+#include <bitset>
+
+using namespace std;
+
+#define NONE -1
+
+const int MOD = 1e9 + 7;
+const int dr[] = {0, -1, 0, 1};
+const int dc[] = {-1, 0, 1, 0};
+
+vector<int> countPrimes(const int& N, const int& M) {
+	vector<bool> isPrime(M + 1, true);
+	vector<int> res;
+
+	isPrime[1] = false;
+
+	int n = 2;
+	for (int i = 2; i <= M; i++) {
+		if (!isPrime[i])
+			continue;
+
+		for (int j = 2 * i; j <= M; j += i)
+			isPrime[j] = false;
+	}
+
+	for (int i = N; i <= M; i++)
+		if (isPrime[i])
+			res.push_back(i);
+	
+	sort(res.begin(), res.end());
+	return res;
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int N, M;
+	cin >> N >> M;
+	vector<int> primes = countPrimes(N, M);
+	for (int n : primes)
+		cout << n << '\n';
+
+
+	return 0;
+}
---
+++ Sieve of Eratosthenes/boj/6588/src.cpp
@@ -0,0 +1,64 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <math.h>
+#include <bitset>
+#include <stack>
+#include <queue>
+#include <math.h>
+#include <deque>
+#include <map>
+#include <set>
+#include <unordered_set>
+#include <unordered_map>
+#include <limits.h>
+#include <numeric>
+#include <functional>
+#include <sstream>
+#include <bitset>
+
+using namespace std;
+
+#define NONE -1
+
+const int MOD = 1e9 + 7;
+const int dr[] = {0, -1, 0, 1};
+const int dc[] = {-1, 0, 1, 0};
+
+vector<bool> isPrime(1e6 + 1, true);
+
+void findPrimes() {
+	isPrime[0] = isPrime[1] = false;
+
+	for (int i = 2; i <= 1e6; i++) {
+		if (!isPrime[i])
+			continue;
+
+		for (int j = 2 * i; j <= 1e6; j += i)
+			isPrime[j] = false;
+	}
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int N;
+	findPrimes();
+
+	while (true) {
+		cin >> N;
+		if (N == 0)
+			break;
+
+		for (int i = 3; i <= N / 2; i += 2) {
+			if (isPrime[N - i]) {
+				cout << N << " = " << i << " + " << N - i << '\n';
+				break;
+			}
+		}
+	}
+
+	return 0;
+}
