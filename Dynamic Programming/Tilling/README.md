7e168b342b8370f73ccf7432be1c63e9787f753e
seonpilKim <kimsunpil123@naver.com>
2021-10-03 17:29:44 +0900

    Solve 11726

--- Dynamic Programming/Tilling/README.md
+++ Dynamic Programming/Tilling/README.md
@@ -8,4 +8,5 @@
             > `dp[1]` -> 1 x 2 타일만 채울 수 있으므로 경우의 수는 1<br>
             > `dp[2]` -> 1 x 2 타일 두 개 혹은 2 x 1 타일 두 개를 채울 수 있으므로 경우의 수는 2
 ## 📂종류
+- [벽: `2 x N` | 타일: `2 x 1, 1 x 2`](https://github.com/seonpilKim/Algorithm/tree/master/Dynamic%20Programming/Tilling/boj/11726)
 - [벽: `3 x N` | 타일: `2 x 1, 1 x 2`](https://github.com/seonpilKim/Algorithm/tree/master/Dynamic%20Programming/Tilling/boj/2133)
---
+++ Dynamic Programming/Tilling/boj/11726/src.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <math.h>
+#include <bitset>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <map>
+#include <set>
+#include <unordered_set>
+#include <unordered_map>
+#include <limits.h>
+#include <numeric>
+#include <functional>
+#include <sstream>
+
+using namespace std;
+
+const long long MOD = 1e9 + 7;
+const long long INF = 9e9;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int N;
+	cin >> N;
+
+	vector<int> dp(N + 1);
+	dp[0] = 1;
+	dp[1] = 1;
+	dp[2] = 2;
+
+	for (int i = 3; i <= N; i++)
+		dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+
+	cout << dp[N];
+
+	return 0;
+}
