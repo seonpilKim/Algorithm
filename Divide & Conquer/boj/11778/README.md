0d3c83b5ffc275051ff432f04feb1626b9685842
seonpilKim <ksp970306@gmail.com>
2021-10-09 15:38:49 +0900

    Solve 11778

---
+++ Divide & Conquer/boj/11778/README.md
@@ -0,0 +1,53 @@
+# [[11778] 피보나치 수와 최대공약수](https://www.acmicpc.net/problem/11778)
+
+![](imgs/1.PNG)
+___
+## 🤔접근
+- `gcd(a, b) = g`일 때, `gcd(F(a), F(b)) = F(g)`임을 이용하자.
+___
+## 💡풀이
+- <b>알고리즘 & 자료구조</b>
+	- `분할 정복을 이용한 행렬 거듭 제곱`
+	- `유클리드 호제법`
+- <b>구현</b>
+___
+## ✍ 피드백
+___
+## 💻 핵심 코드
+```c++
+typedef vector<vector<long long>> matrix;
+
+matrix operator* (const matrix& A, const matrix& B) {
+	int n = A.size();
+	matrix res(n, vector<long long>(n));
+
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < n; j++)
+			for (int k = 0; k < n; k++)
+				res[i][j] = (res[i][j] + A[i][k] * B[k][j]) % MOD;
+
+	return res;
+}
+
+long long getFibonacci(long long n) {
+	matrix res = {{1, 0}, {0, 1}};
+	matrix c = {{1, 1}, {1, 0}};
+
+	while (n) {
+		if (n % 2)
+			res = res * c;
+		c = c * c;
+		n /= 2;
+	}
+
+	return res[0][1];
+}
+
+int main() {
+	...
+
+	cout << (getFibonacci(b + 2) - getFibonacci(a + 1) + MOD) % MOD;
+
+	...
+}
+```
---
+++ Divide & Conquer/boj/11778/src.cpp
@@ -0,0 +1,72 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <math.h>
+#include <bitset>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <map>
+#include <set>
+#include <unordered_set>
+#include <unordered_map>
+#include <limits.h>
+#include <numeric>
+#include <functional>
+#include <sstream>
+
+using namespace std;
+
+const long long MOD = 1e9 + 7;
+const long long INF = 9e9;
+
+const int dr[] = {1, 0, -1, 0};
+const int dc[] = {0, 1, 0, -1};
+
+typedef vector<vector<long long>> matrix;
+
+matrix operator* (const matrix& A, const matrix& B) {
+	int n = A.size();
+	matrix res(n, vector<long long>(n));
+
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < n; j++)
+			for (int k = 0; k < n; k++)
+				res[i][j] = (res[i][j] + A[i][k] * B[k][j]) % MOD;
+
+	return res;
+}
+
+long long getFibonacci(long long n) {
+	matrix res = {{1, 0}, {0, 1}};
+	matrix c = {{1, 1}, {1, 0}};
+
+	while (n) {
+		if (n % 2)
+			res = res * c;
+		c = c * c;
+		n /= 2;
+	}
+
+	return res[0][1];
+}
+
+long long getGcd(long long a, long long b) {
+	if (b == 0)
+		return a;
+
+	return getGcd(b, a % b);
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	long long a, b;
+	cin >> a >> b;
+
+	cout << getFibonacci(getGcd(a, b));
+
+	return 0;
+}
