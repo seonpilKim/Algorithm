9526fd4a81fe898309a848c24eda45889b57a1f9
seonpilKim <ksp970306@gmail.com>
2021-12-23 18:41:23 +0900

    Solve: [G4] 크게 만들기

---
+++ Greedy/boj/2812/src.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <math.h>
+#include <bitset>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <map>
+#include <set>
+#include <unordered_set>
+#include <unordered_map>
+#include <limits.h>
+#include <numeric>
+#include <functional>
+#include <sstream>
+#include <cctype>
+
+using namespace std;
+
+const long long MOD = 1e9 + 7;
+const long long INF = 1e9;
+
+const int dr[] = {1, 0, -1, 0, 1, 1, -1, -1};
+const int dc[] = {0, 1, 0, -1, 1, -1, 1, -1};
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int N, K;
+	string num;
+	cin >> N >> K >> num;
+
+	string ans = "";
+	for (int i = 0; i < N; i++) {
+		while (K && !ans.empty() && ans.back() < num[i]) 
+			ans.pop_back(), K--;	
+		ans += num[i];
+	}
+	
+	cout << ans;
+
+	return 0;
+}
